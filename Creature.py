e140a2c1e04f831738424cfce2bf62cf656a740f
Natalie Milian <milnats2001@gmail.com>
2023-03-14 15:24:19 -0400

    Added comment

--- Creature.py
+++ Creature.py
@@ -4,9 +4,9 @@ import logging
 from mongoengine import *
 from filler_creatures import creatures, abilities
 
-
 logging.basicConfig(level=logging.DEBUG)
 
+# Ojala mi internet fuera asi
 
 class Ability(Document):
     name = StringField(required=True, unique=True)
